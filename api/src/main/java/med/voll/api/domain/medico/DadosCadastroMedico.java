1f03ab7aef5d3871b63dd35638b6f6ea81065f76
Maicon Jordan <maiconplus@gmail.com>
2024-08-27 21:56:32 -0300

    Tratamento de erros
    Devolvendo erros tratado nas requisições

--- api/src/main/java/med/voll/api/domain/medico/DadosCadastroMedico.java
+++ api/src/main/java/med/voll/api/domain/medico/DadosCadastroMedico.java
@@ -1,19 +1,19 @@
-package med.voll.api.medico;
+package med.voll.api.domain.medico;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import med.voll.api.endereco.DadosEndereco;
+import med.voll.api.domain.endereco.DadosEndereco;
 
 public record DadosCadastroMedico(
         @NotBlank
         String nome,
-        @NotBlank
+        @NotBlank(message = "Email é obrigatório")
+        @Email
         String email,
         @NotBlank
-        @Email
         String telefone,
         @NotBlank
         @Pattern(regexp = "\\d{4,6}")
